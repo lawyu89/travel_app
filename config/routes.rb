16dcef267bee316bc3c9abf1f6fc4410e797e8ec
EmilyGerngross <emilygerngross@gmail.com>
2015-05-08 14:45:06 -0700

    add routes and city/attraction controllers

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,18 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :sessions
+
+  resources :cities do
+    resources :attractions
+  end
+
+  resources :users do
+    resources :cities do
+      resources :attractions
+    end
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
