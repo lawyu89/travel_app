8ce0e73a59cb9b06fa4c9c38b82cba232f58c3ec
EmilyGerngross <emilygerngross@gmail.com>
2015-05-09 12:09:28 -0700

    add mycities/city_id/attractions page functionalities

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,11 +7,12 @@ class AttractionsController < ApplicationController
 
   def my_attractions_index
     if current_user
+      @user = @current_user
       @city = City.where(id: params[:city_id]).first
+      find_my_attractions(@city)
     else
-
+      redirect_to cities_path
     end
-
   end
 
 end
--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -1,2 +1,14 @@
 module AttractionsHelper
+
+  # helper method that finds the "liked" join tables and then returns
+  # the attraction objects for each "liked" attraction
+  def find_my_attractions(city)
+    user_attractions = @user.user_attractions.where(city_id: @city.id, preference: true)
+    @attractions = []
+    user_attractions.each do |row|
+      @attractions << Attraction.where(id: row.attraction_id).first
+    end
+    @attractions
+  end
+
 end
