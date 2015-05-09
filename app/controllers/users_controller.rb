b3172cec96ffa358d9d097b58086db5a3cb2dde0
missling <ling.giang@gmail.com>
2015-05-09 14:24:58 -0700

    add sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
   
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
+      username: params[:user][:username],
+      email: params[:user][:email],
+      password: params[:user][:password]
+      )
+
+    if @user.save
+      log_in(@user)
+      redirect_to cities_path
+    else
+      render :new
+    end
+  end
+  
 end
