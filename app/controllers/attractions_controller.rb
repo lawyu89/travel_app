ba4e3fe1aeacc5fa2371319c7089b630a1ee2668
EmilyGerngross <emilygerngross@gmail.com>
2015-05-09 11:32:28 -0700

    add partial mycities/id/attractions setup

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,4 +4,14 @@ class AttractionsController < ApplicationController
     p city = City.where(id: params[:city_id]).first
     @attractions = city.attractions
   end
+
+  def my_attractions_index
+    if current_user
+      @city = City.where(id: params[:city_id]).first
+    else
+
+    end
+
+  end
+
 end
