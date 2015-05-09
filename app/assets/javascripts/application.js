ba72903b96df9a1b9bd91c9abadd197b0ed8bef1
Michael Avtonomoff <m.avt@icloud.com>
2015-05-09 12:03:56 -0700

    Created local hammerjs file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-var hammer = new Hammer($('body'))
+//= require hammer
--- app/assets/javascripts/hammertime.js
+++ app/assets/javascripts/hammertime.js
@@ -1,23 +1,21 @@
-var el = document.getElementsByClassName('attraction');
+$(document).ready(function(){
+  var myElement = $('#attraction0');
 
 // We create a manager object, which is the same as Hammer(), but without the presetted recognizers.
-var hammer = new Hammer(el)
-var mc = new Hammer.Manager(el);
-
-
-// Tap recognizer with minimal 2 taps
-mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
-// Single tap recognizer
-mc.add( new Hammer.Tap({ event: 'singletap' }) );
-
-
-// we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
-mc.get('doubletap').recognizeWith('singletap');
-// we only want to trigger a tap, when we don't have detected a doubletap
-mc.get('singletap').requireFailure('doubletap');
-
-
-mc.on("singletap doubletap", function(ev) {
-    // el.textContent += ev.type +" ";
-    console.log('You just did a ' + ev.type)
-});
+  var mc = new Hammer.Manager(myElement);
+//
+// // Default, tap recognizer
+// mc.add( new Hammer.Tap() );
+//
+// // Tap recognizer with minimal 4 taps
+// mc.add( new Hammer.Tap({ event: 'quadrupletap', taps: 4 }) );
+//
+// // we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
+// // the tap event will be emitted on every tap
+// mc.get('quadrupletap').recognizeWith('tap');
+//
+//
+// mc.on("tap quadrupletap", function(ev) {
+//     myElement.textContent += ev.type +" ";
+// });
+})
