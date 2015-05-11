3e924ae451f73974e2a9c1ef7a17308cc672a81e
Lawrence Yu <lawrenceyu1@gmail.com>
2015-05-11 14:42:27 -0700

    Adjust seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,7 @@ attractions.each_with_index do |attraction,index|
   next if attraction['description'].length <1
   a = Attraction.new(name: attraction['name'], rank: attraction['rank'].to_i, query_slug: attraction['query_slug'], description: attraction['description'], top_quote: attraction['top_quote'])
   if index < 64
-    result = BingSearch.image(attraction['name']+' Bangkok', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Bangkok', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -62,7 +62,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Bangkok').attractions << a
     a.save
   elsif index < 136
-    result = BingSearch.image(attraction['name']+' Beirut', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Beirut', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -71,7 +71,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Beirut').attractions << a
     a.save
   elsif index < 155
-    result = BingSearch.image(attraction['name']+' Budapest', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Budapest', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -80,7 +80,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Budapest').attractions << a
     a.save
   elsif index < 214
-    result = BingSearch.image(attraction['name']+' Cape Town', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Cape Town', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -89,7 +89,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Cape Town').attractions << a
     a.save
   elsif index <285
-    result = BingSearch.image(attraction['name']+' Detroit', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Detroit', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -98,7 +98,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Detroit').attractions << a
     a.save
   elsif index <347
-    result = BingSearch.image(attraction['name']+' Florence', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Florence', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -107,7 +107,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Florence').attractions << a
     a.save
   elsif index <412
-    result = BingSearch.image(attraction['name']+' Istanbul', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Istanbul', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -116,7 +116,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Istanbul').attractions << a
     a.save
   elsif index <482
-    result = BingSearch.image(attraction['name']+' Krakow', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Krakow', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -125,7 +125,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Krakow').attractions << a
     a.save
   elsif index <556
-    result = BingSearch.image(attraction['name']+' Kyoto', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Kyoto', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -134,7 +134,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Kyoto').attractions << a
     a.save
   elsif index <633
-    result = BingSearch.image(attraction['name']+' Paris', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Paris', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -143,7 +143,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Paris').attractions << a
     a.save
   elsif index <699
-    result = BingSearch.image(attraction['name']+' Rio de Janeiro', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Rio de Janeiro', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -152,7 +152,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Rio de Janeiro').attractions << a
     a.save
   elsif index <757
-    result = BingSearch.image(attraction['name']+' San Francisco', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' San Francisco', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -161,7 +161,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'San Francisco').attractions << a
     a.save
   elsif index <822
-    result = BingSearch.image(attraction['name']+' Sydney', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Sydney', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -170,7 +170,7 @@ attractions.each_with_index do |attraction,index|
     City.find_by(name: 'Sydney').attractions << a
     a.save
   elsif index <897
-    result = BingSearch.image(attraction['name']+' Sydney', filters: [:square, :medium]).first
+    result = attraction['photo_url'] || BingSearch.image(attraction['name']+' Sydney', filters: [:square]).first
     if result.nil?
       a.photo_url = "ADD ME MOFO"
     else
@@ -201,7 +201,7 @@ end
 #   if parsed1['results']['collection1'][i]['description'].nil?
 #     i+=1
 #   elsif i<20
-#     result = BingSearch.image(parsed1['results']['collection1'][i]['name']+' paris', filters: [:square]).first.media_url
+#     result = attraction['photo_url'] || BingSearch.image(parsed1['results']['collection1'][i]['name']+' paris', filters: [:square]).first.media_url
 #     a = Attraction.create(name: parsed1['results']['collection1'][i]['name'], address: parsed1['results']['collection1'][i]['address'], description: parsed1['results']['collection1'][i]['description'], photo_url: result, query_slug: parsed2['rows'][i]['wiki+city'], rank: parsed2['rows'][i]['base_rank'].to_i, top_quote:  parsed2['rows'][i]['top_quote'])
 #     paris.attractions << a
 #     i+=1
