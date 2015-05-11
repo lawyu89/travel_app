76eba91857f645321acdcfd7965efd144480ff94
Lawrence Yu <lawrenceyu1@gmail.com>
2015-05-11 14:20:18 -0700

    Add login and sign up error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
+    @error
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
       redirect_to cities_path
     else
       # Create an error message.
-      flash.now[:danger] = 'Invalid email/password combination.'
+      @error = 'Invalid email/password combination.'
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(
-      first_name: params[:user][:first_name],
-      last_name: params[:user][:last_name],
       username: params[:user][:username],
       email: params[:user][:email],
       password: params[:user][:password]
@@ -20,5 +18,5 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
 end
