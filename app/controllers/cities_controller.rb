23faaef8c052e7546ee7a9ac011f30096b693946
EmilyGerngross <emilygerngross@gmail.com>
2015-05-11 11:53:47 -0700

    add header links to my cities page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,16 +6,6 @@ class CitiesController < ApplicationController
   end
 
   def my_cities_index
-    @user = current_user
-    @cities = @user.cities
-  end
-
-  def test
-    @user = current_user
-    # @cities = @user.cities
-  end
-
-  def test_my_cities_index
     if current_user
       @user = current_user
       @cities = @user.cities
@@ -23,5 +13,19 @@ class CitiesController < ApplicationController
       redirect_to cities_path
     end
   end
+
+  # def test
+  #   @user = current_user
+  #   # @cities = @user.cities
+  # end
+
+  # def test_my_cities_index
+  #   if current_user
+  #     @user = current_user
+  #     @cities = @user.cities
+  #   else
+  #     redirect_to cities_path
+  #   end
+  # end
 
 end
