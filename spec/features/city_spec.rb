309942db3d53a33540c2c064e785fa2b99432ae6
grandnumber <thegrandnumber@gmail.com>
2015-05-11 17:29:32 -0700

    successful click check

--- spec/features/city_spec.rb
+++ spec/features/city_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
 
-# describe "the questions paths", :js => true do
-#   let(:question) {FactoryGirl.create :question}
 
-#   scenario "homepage renders with Question on it" do
-#     visit '/'
-#     expect(page).to have_content('The amazing stack of Qs')
-#     save_and_open_page
-#     # sleep 5
-
-#   end
-# end 
-
+# to run one test: be rspec spec/features/city_spec.rb:50
 
 describe "the home page/ city index route", :js => true do
-  # @bangkok = City.create(name:"Bangkok")
-  # @beirut = City.create(name:"Beirut")
-  # @budapest = City.create(name:"Budapest")
-  # @cape_town = City.create(name:"Cape Town")
-  # @detroit = City.create(name:"Detroit")
-  # @florence = City.create(name:"Florence")
-  # @kyoto = City.create(name:"Kyoto")
-  # @rio = City.create(name:"Rio de Janeiro")
-  # @san_fran = City.create(name:"San Francisco")
-  # @sydney = City.create(name:"Sydney")
-  # @vienna = City.create(name:"Vienna")
-  
-
-  # @attraction1 = Attraction.create(name:"test1", city_id: @bangkok.id)
-  # @attraction2 = Attraction.create(name:"test2", city_id: @bangkok.id)
-  # @attraction3 = Attraction.create(name:"test3", city_id: @bangkok.id)
-  # @attraction4 = Attraction.create(name:"test4", city_id: @bangkok.id)
-  # @attraction5 = Attraction.create(name:"test5", city_id: @bangkok.id)
+  before(:each) do 
+    @bangkok = City.create(name:"Bangkok")
+    @beirut = City.create(name:"Beirut")
+    @budapest = City.create(name:"Budapest")
+    @cape_town = City.create(name:"Cape Town")
+    @detroit = City.create(name:"Detroit")
+    @florence = City.create(name:"Florence")
+    @kyoto = City.create(name:"Kyoto")
+    @rio = City.create(name:"Rio de Janeiro")
+    @san_fran = City.create(name:"San Francisco")
+    @sydney = City.create(name:"Sydney")
+    @vienna = City.create(name:"Vienna")
+    
+    @attraction1 = Attraction.create(name:"test1", city_id: @bangkok.id)
+    @attraction2 = Attraction.create(name:"test2", city_id: @bangkok.id)
+    @attraction3 = Attraction.create(name:"test3", city_id: @bangkok.id)
+    @attraction4 = Attraction.create(name:"test4", city_id: @bangkok.id)
+    @attraction5 = Attraction.create(name:"test5", city_id: @bangkok.id)
+  end 
 
   scenario "homepage renders with city pictures on it " do
     visit '/cities'
@@ -40,15 +31,17 @@ describe "the home page/ city index route", :js => true do
   end
 
 
-  scenario "city attractions has div " do
-    @bangkok = City.create(name:"Bangkok")
-    @attraction1 = Attraction.create(name:"test1", city_id: @bangkok.id)
-    @attraction2 = Attraction.create(name:"test2", city_id: @bangkok.id)
-    @attraction3 = Attraction.create(name:"test3", city_id: @bangkok.id)
-    @attraction4 = Attraction.create(name:"test4", city_id: @bangkok.id)
-    @attraction5 = Attraction.create(name:"test5", city_id: @bangkok.id)
+  scenario "city attractions has div & renders correct content " do
     visit city_attractions_path(@bangkok)
     expect(page).to have_content('test1')
+    save_and_open_page 
+  end
+
+  scenario "clicking RHS button moves to the second attraction " do
+    visit city_attractions_path(@bangkok)
+    save_and_open_page 
+    find_by_id('leftb').click
+    expect(page).to have_content('test2')
     save_and_open_page 
   end
 
