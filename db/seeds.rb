32b877f9e1009b8dfb686fbf15335813507f2016
EmilyGerngross <emilygerngross@gmail.com>
2015-05-12 18:13:38 -0700

    add gem, remove unnecessary require

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-require 'JSON'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-
-require 'bing-search'
 
 City.destroy_all
 Attraction.destroy_all
