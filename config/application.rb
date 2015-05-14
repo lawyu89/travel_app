51fca25edfddf1bd2b4421b43247684492d7122e
Lawrence Yu <lawrenceyu1@gmail.com>
2015-05-14 10:49:16 -0700

    Add config setting

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 #Dotenv::Railtie.load
 module Travel
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
